15b963feb6935fa5c24a97b1c0afac4bae137409
Temlin Olivér <git@teml.in>
2023-10-15 21:34:49 +0200

    fix negative direction, debounce, fast return

--- ribbon_cutter.ino
+++ ribbon_cutter.ino
@@ -9,13 +9,21 @@
 Servo arm;
 
 // params
-#define servo_start 170
-#define servo_end 30
-#define servo_step -1
+#define angle_down 170
+#define angle_up 30
 #define speed_max 2
-#define rest_time 200
-uint8_t pos = servo_start;
-const uint8_t servo_steps = servo_end - servo_start;
+#define rest_top 250
+#define rest_bottom 1000
+
+// calculated
+#if angle_down < angle_up
+#define angle_dir 1
+const int servo_steps = angle_up - angle_down;
+#else
+const int servo_steps = angle_down - angle_up;
+#define angle_dir -1
+#endif
+int pos = angle_down;
 unsigned long step_time = 2048 / servo_steps;
 
 void setup() {
@@ -26,12 +34,20 @@ void setup() {
   digitalWrite(led, LOW);
   digitalWrite(heat, LOW);
   arm.attach(servo);
-  arm.write(pos);
-  delay(rest_time);
+  arm.write(angle_down);
+  delay(rest_bottom);
   arm.detach();
 }
 
 void loop() {
+  // Wait for button release
+  while (true) {
+    if (digitalRead(button) == HIGH)
+      break;
+    else
+      delay(1);
+  }
+
   // Wait for button press
   while (true) {
     if (digitalRead(button) == LOW)
@@ -40,31 +56,29 @@ void loop() {
       delay(1);
   }
 
-  //Read pot to set speed
-  step_time = analogRead(pot) * speed_max / servo_steps;
+  // Cycle start
+  digitalWrite(led, HIGH);
+
+  // Read pot to set speed
+  step_time = ((unsigned long) analogRead(pot) * (unsigned long)speed_max) / (unsigned long)servo_steps;
 
   // Turn on heat, start servo
-  digitalWrite(led, HIGH);
   digitalWrite(heat, HIGH);
   arm.attach(servo);
 
   // Raise arm
-  for (pos = servo_start; pos <= servo_end; pos += servo_step) {
+  for (pos = angle_down; pos != angle_up; pos += angle_dir) {
     arm.write(pos);
     delay(step_time);
   }
 
-  // Wait a little
-  delay(rest_time);
-
-  // Lower arm
-  for (pos = servo_end; pos >= servo_start; pos -= servo_step) {
-    arm.write(pos);
-    delay(step_time);
-  }
-
-  // Turn off heat, let servo rest
-  digitalWrite(led, LOW);
+  // Turn off heat, wait a little
   digitalWrite(heat, LOW);
+  delay(rest_top);
+
+  // Lower arm, wait  a little, let servo rest
+  arm.write(angle_down);
+  delay(rest_bottom);
   arm.detach();
+  digitalWrite(led, LOW);
 }

20aaa3aa93fccfcf3bbef777bffbf3541d5b636b
Temlin Olivér <git@teml.in>
2023-10-15 18:50:55 +0200

    detach servo when unused

--- ribbon_cutter.ino
+++ ribbon_cutter.ino
@@ -7,24 +7,28 @@
 #define heat A3
 #define led LED_BUILTIN
 Servo arm;
-uint8_t pos = 0;
 
 // params
 #define servo_start 170
 #define servo_end 30
 #define servo_step -1
 #define speed_max 2
+#define rest_time 200
+uint8_t pos = servo_start;
 const uint8_t servo_steps = servo_end - servo_start;
 unsigned long step_time = 2048 / servo_steps;
 
 void setup() {
-  arm.attach(servo);
   pinMode(button, INPUT);
   pinMode(pot, INPUT);
   pinMode(led, OUTPUT);
   pinMode(heat, OUTPUT);
   digitalWrite(led, LOW);
   digitalWrite(heat, LOW);
+  arm.attach(servo);
+  arm.write(pos);
+  delay(rest_time);
+  arm.detach();
 }
 
 void loop() {
@@ -39,19 +43,19 @@ void loop() {
   //Read pot to set speed
   step_time = analogRead(pot) * speed_max / servo_steps;
 
-  // Turn on heat
+  // Turn on heat, start servo
   digitalWrite(led, HIGH);
   digitalWrite(heat, HIGH);
+  arm.attach(servo);
 
   // Raise arm
   for (pos = servo_start; pos <= servo_end; pos += servo_step) {
     arm.write(pos);
     delay(step_time);
-
   }
 
   // Wait a little
-  delay(200);
+  delay(rest_time);
 
   // Lower arm
   for (pos = servo_end; pos >= servo_start; pos -= servo_step) {
@@ -59,7 +63,8 @@ void loop() {
     delay(step_time);
   }
 
-  // Turn off heat
+  // Turn off heat, let servo rest
   digitalWrite(led, LOW);
   digitalWrite(heat, LOW);
+  arm.detach();
 }
